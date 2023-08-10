8f17de3037f245a5148561f2cc9bd52be2d55c2f
jimenarsss <jimena@melix.net>
2023-08-10 15:01:56 +0200

    Delete test.py

--- tests/test.py
+++
@@ -1,7 +0,0 @@
-
-
-# /form route
-"ip:port/form"
-
-# /query route
-"ip:port/query?query=Moralisme%20progressiste%20et%20pratiques%20punitives%20dans%20la%20lutte%20contre%20les%20violences%20sexistes&hits=3"
